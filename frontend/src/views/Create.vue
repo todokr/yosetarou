3ed1f855f99536a7a2cb4932eec7797328c32ad6
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-08-03 00:01:17 +0900

    fix create page

--- frontend/src/views/Create.vue
+++ frontend/src/views/Create.vue
@@ -1,33 +1,21 @@
 <template>
-  <div class="create">
-    <div class="container">
-      <div class="step">
-        <sui-step-group ordered>
-          <sui-step active>
-            <sui-step-content>
-              <sui-step-title>デザインを選ぶ</sui-step-title>
-            </sui-step-content>
-          </sui-step>
-          <sui-step>
-            <sui-step-content>
-              <sui-step-title>基本情報を入力する</sui-step-title>
-            </sui-step-content>
-          </sui-step>
-          <sui-step>
-            <sui-step-content>
-              <sui-step-title>みんなでメッセージを書く</sui-step-title>
-            </sui-step-content>
-          </sui-step>
-          <sui-step>
-            <sui-step-content>
-              <sui-step-title>寄せ書きを贈る</sui-step-title>
-            </sui-step-content>
-          </sui-step>
-        </sui-step-group>
-      </div>
-    </div>
+<div class="create">
+  <div class="container">
+    <Step />
+  </div>
   </div>
 </template>
+
+<script>
+import Step from '@/components/Step.vue'
+
+export default {
+  name: 'Create',
+  components: {
+    Step
+  },
+}
+</script>
 
 <style lang="scss" scoped>
   .create {
